d4ebfc89d02a9a02c2569a9f0e198067639c23cb
xxzz9978 <xxzz9978@naver.com>
2024-08-16 23:07:59 +0900

    의존성 재추가

--- pj/Client/src/App.js
+++ pj/Client/src/App.js
@@ -1,46 +1,24 @@
-import React, { useState, useEffect } from "react";
-import TestComponent from "./test/TestComponent";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import MyProfile from "./component/mypage/MyProfile";
 import HomePage from "./component/HomePage";
 import Chat from './Chat/Chat';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-    const [testComponent, setTestComponent] = useState([]);
-
-    useEffect(() => {
-        const callApi = async () => {
-            const response = await fetch("/api/test");
-            const body = await response.json();
-            return body;
-        };
-        callApi()
-            .then((res) => setTestComponent(res))
-            .catch((err) => console.log(err));
-    }, []);
-
     return (
         <Router>
-            <MainContent testComponent={testComponent} />
+            <MainContent />
         </Router>
     );
 }
 
-function MainContent({ testComponent }) {
+function MainContent() {
     const location = useLocation();
 
     return (
         <div className="App">
-            {/* /chat경로가 아니면 다른컴포넌트 호출 */}
-            {location.pathname !== "/chat" && (
-                <>
-                    {testComponent && testComponent.map((u) => (
-                        <TestComponent key={u.user_no} {...u} />
-                    ))}
-                    {/* 내정보 프로필 카드의 미리보기 */}
-                    <MyProfile />
-                </>
-            )}
+            {/* /chat 경로가 아니면 MyProfile 컴포넌트를 호출 */}
+            {location.pathname !== "/chat" && <MyProfile />}
 
             {/* 라우팅 처리 */}
             <Routes>
