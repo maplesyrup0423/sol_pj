4498b7838a89a80e07e1ca58d6ce24b699737e32
xxzz9978 <xxzz9978@naver.com>
2024-08-16 21:11:59 +0900

    머지후 컴포넌트 동시실행정리

--- pj/Client/src/App.js
+++ pj/Client/src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import TestComponent from "./test/TestComponent";
 import MyProfile from "./component/mypage/MyProfile";
-import HomePage from "./component/HomePage"
+import HomePage from "./component/HomePage";
 import Chat from './Chat/Chat';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-    const [testComponent, seTestComponent] = useState([]);
+    const [testComponent, setTestComponent] = useState([]);
 
     useEffect(() => {
         const callApi = async () => {
@@ -15,24 +15,40 @@ function App() {
             return body;
         };
         callApi()
-            .then((res) => seTestComponent(res))
+            .then((res) => setTestComponent(res))
             .catch((err) => console.log(err));
     }, []);
-    
+
+    return (
+        <Router>
+            <MainContent testComponent={testComponent} />
+        </Router>
+    );
+}
+
+function MainContent({ testComponent }) {
+    const location = useLocation();
 
     return (
         <div className="App">
-            {/* {testComponent
-                ? testComponent.map((u) => (
-                      <TestComponent key={u.user_no} {...u} />
-                  ))
-                : ""} */}
-            {/* 내정보 프로필 카드의 미리보기 */}
-            {<MyProfile />}
-            {/* <HomePage /> */}
+            {/* /chat경로가 아니면 다른컴포넌트 호출 */}
+            {location.pathname !== "/chat" && (
+                <>
+                    {testComponent && testComponent.map((u) => (
+                        <TestComponent key={u.user_no} {...u} />
+                    ))}
+                    {/* 내정보 프로필 카드의 미리보기 */}
+                    <MyProfile />
+                </>
+            )}
+
+            {/* 라우팅 처리 */}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/chat" element={<Chat />} />
+            </Routes>
         </div>
     );
- 
 }
 
 export default App;
