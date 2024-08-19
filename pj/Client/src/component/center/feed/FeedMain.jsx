5bcd38a117aa7691e2becc99430263ead4625277
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-19 19:01:51 +0900

    인기 /최신

--- pj/Client/src/component/center/feed/FeedMain.jsx
+++ pj/Client/src/component/center/feed/FeedMain.jsx
@@ -1,10 +1,18 @@
 import "./FeedMain.css";
 import Feed from "./Feed";
 import Feeds from "./Feeds";
+
 function FeedMain() {
   return (
     <div className="feed_main">
-      <div className="order">인기 / 최신</div>
+      <div className="order">
+        <div className="popularity">
+          <div>인기</div>
+        </div>
+        <div className="Latest">
+          <div>최신</div>
+        </div>
+      </div>
 
       <div className="posting">글쓰기 부분</div>
 
