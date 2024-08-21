017d5d9aee2750755fb4cf8e9edc0c840e59ad04
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-21 21:18:34 +0900

    주석 추가

--- pj/controllers/myInfo.js
+++ pj/controllers/myInfo.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// 로그인한 유저의 user_no을 이용해서 닉네임, 프로필 이미지 가져오기
+// 로그인한 유저의 user_no을 이용해서 닉네임, 프로필 이미지, 유저번호, 유저 아이디 가져오기
 //! 현재 로그인한 유저 판별이 불가능하기에 user_no이 1인 유저 정보를 가져와서 테스트 함
 module.exports = (conn) => {
   router.get("/api/myInfo", (req, res) => {
@@ -24,3 +24,4 @@ module.exports = (conn) => {
 };
 
 //todo "/api/myInfo/:user_no"  const userNo = req.params.user_no; 로 바꾸면 /api/myInfo/1와 같이 요청할 수 있습니다.
+//todo 컨트롤러 파일 좀 적게 쓰는 방법 구상하기 =>Knex 사용
