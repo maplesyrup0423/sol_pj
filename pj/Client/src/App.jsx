f81407cff167a24fc8f0cc3509f167765551ff1d
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-28 01:53:39 +0900

    중앙부분 라우터 처리 app

--- pj/Client/src/App.jsx
+++ pj/Client/src/App.jsx
@@ -4,14 +4,47 @@ import "./App.css";
 import LoginPage, { action as loginAction } from "./components/pages/Login";
 import IntroPage from "./components/pages/IntroLayout/Intro";
 import HomePage from "./components/pages/homeLayout/Home";
-import BoardPage from "./components/pages/homeLayout/center/feed/BoardPage";
+import FeedMain from "./components/pages/homeLayout/center/feed/FeedMain";
+import UserProfile from "./components/pages/homeLayout/center/feed/UserProfile";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [myInfo, setMyInfo] = useState([]);
+  useEffect(() => {
+    const myInfoCallApi = async () => {
+      try {
+        const response = await fetch("/api/userProfileInfo");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setMyInfo(data);
+      } catch (err) {
+        console.error("Fetch error:", err);
+        setMyInfo([]); // 오류가 발생할 경우 빈 배열로 설정
+      }
+    };
+
+    myInfoCallApi();
+  }, []);
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-      children: [],
+      element: <HomePage myInfo={myInfo} />,
+      children: [
+        {
+          path: "/",
+          element: <FeedMain myInfo={myInfo} />, //처음에 보일 화면 아직 처리 안함
+        },
+        {
+          path: "/post/:boardId",
+          element: <FeedMain myInfo={myInfo} />, // 게시판 ID에 따라 데이터를 처리할 페이지
+        },
+        {
+          path: "/myPage",
+          element: <UserProfile myInfo={myInfo} />, // 마이페이지
+        },
+      ],
     },
     {
       path: "/login",
@@ -21,10 +54,6 @@ function App() {
     {
       path: "/intro",
       element: <IntroPage />,
-    },
-    {
-      path: "/post/:boardId",
-      element: <BoardPage />, // 게시판 ID에 따라 데이터를 처리할 페이지
     },
   ]);
 
