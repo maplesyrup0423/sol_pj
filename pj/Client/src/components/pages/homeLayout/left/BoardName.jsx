dc9dee60a3630ef0504c9d00563513b4e7ab574d
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-28 02:08:16 +0900

    게시판 선택 시 스타일 변경

--- pj/Client/src/components/pages/homeLayout/left/BoardName.jsx
+++ pj/Client/src/components/pages/homeLayout/left/BoardName.jsx
@@ -1,5 +1,6 @@
 import "./BoardName.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom"; // NavLink를 가져옵니다.
+
 function BoardName(props) {
   return (
     <li key={props.board_info_id}>
@@ -7,14 +8,21 @@ function BoardName(props) {
         <div className="gameInfo gameImg">
           <img src={props.board_img} alt="" />
         </div>
-        <Link to={`/post/${props.board_info_id}`}>
+        <NavLink
+          to={`/post/${props.board_info_id}`}
+          style={({ isActive }) => ({
+            fontWeight: isActive ? "bold" : "normal", // 활성화된 링크에 스타일 적용
+            color: isActive ? "#ffcd19" : "white", // 선택된 링크의 색상
+            textDecoration: "none", // 기본 링크 스타일 제거
+          })}
+        >
           <div className="gameInfo gameTitle">
-            {" "}
             &nbsp; {props.board_info_name}
           </div>
-        </Link>
+        </NavLink>
       </div>
     </li>
   );
 }
+
 export default BoardName;
