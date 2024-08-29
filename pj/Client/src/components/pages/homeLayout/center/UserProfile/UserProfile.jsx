313a4a9ea00182b223fa5fefe0bb64f97c382b43
khkhk1 <gksruf1414@gmail.com>
2024-08-29 22:05:45 +0900

    오늘작업한걸로 변경

--- pj/Client/src/components/pages/homeLayout/center/UserProfile/UserProfile.jsx
+++ pj/Client/src/components/pages/homeLayout/center/UserProfile/UserProfile.jsx
@@ -1,50 +1,79 @@
+import React, { useState } from "react";
 import "./UserProfile.css";
 import BackArrow from "../../../../utills/buttons/BackArrow";
 import BasicButton from "../../../../utills/buttons/BasicButton";
-import UserPosts from "./UserPosts";
 
 function UserProfile({ myInfo }) {
-    console.log("유저 프로필 진입");
-    console.log("myInfo : ", myInfo);
-    console.log("myInfo.nickname : ", myInfo.nickname);
-    return (
-        <>
-            <header>
-                <div className="backArrow">
-                    <BackArrow />
-                </div>
-                <div className="userInfo">
-                    <div className="name">{myInfo[0].nickname}</div>
-                    <div className="nickName">@{myInfo[0].user_id}</div>
-                </div>
-            </header>
-            <main>
-                <div className="userPic">
-                    <div className="pic">
-                        <img
-                            src={myInfo[0].image_url}
-                            alt=""
-                            className="proImg"
-                        />
-                    </div>
-                </div>
-                <div className="userInfo">
-                    <div className="name">{myInfo[0].nickname}</div>
-                    <div className="nickName">@{myInfo[0].user_id}</div>
-                    <div className="introduce">{myInfo[0].introduce}</div>
-                </div>
-                <div className="editProfile">
-                    <BasicButton btnText="프로필수정" />
-                </div>
-            </main>
-            <UserPosts />
-            <UserPosts />
-            <UserPosts />
-            <UserPosts />
-            <UserPosts />
-            <UserPosts />
-            <UserPosts />
-        </>
-    );
+  //현재 선택된 탭 관리
+  const [activeTab, setActiveTab] = useState("posts");
+
+  //switch-posts 클릭시 호출
+  const showPosts = () => {
+    setActiveTab("posts");
+  };
+
+  //switch-comments 클릭시 호출
+  const showComments = () => {
+    setActiveTab("comments");
+  };
+
+  console.log("myInfo : ", myInfo);
+  console.log("myInfo.nickname : ", myInfo.nickname);
+  return (
+    <>
+      <header>
+        <div className="backArrow">
+          <BackArrow />
+        </div>
+        <div className="userInfo">
+          <div className="name">{myInfo[0].nickname}</div>
+          <div className="nickName">@{myInfo[0].user_id}</div>
+        </div>
+      </header>
+      <main>
+        <div className="userContainer">
+          <div className="userPic">
+            <div className="pic">
+              <img src={myInfo[0].image_url} alt="" className="proImg" />
+            </div>
+          </div>
+          <div className="userInfo">
+            <div className="name">{myInfo[0].nickname}</div>
+            <div className="nickName">@{myInfo[0].user_id}</div>
+            <div className="introduce">{myInfo[0].introduce}</div>
+            <div className="editProfile">
+              <BasicButton btnText="프로필수정" />
+            </div>
+          </div>
+        </div>
+        <div className="switch">
+          <div
+            className={`switch-posts ${activeTab === "posts" ? "active" : ""}`}
+            onClick={showPosts}
+          >
+            게시글
+          </div>
+          <div
+            className={`switch-comments ${
+              activeTab === "comments" ? "active" : ""
+            }`}
+            onClick={showComments}
+          >
+            댓글
+          </div>
+        </div>
+        <div className="list">
+          <div className="content">
+            {activeTab === "posts" && (
+              <div className="posts">게시글 누르면 이게 나옴</div>
+            )}
+            {activeTab === "comments" && (
+              <div className="comments">댓글 누르면 이게 나옴</div>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 export default UserProfile;
