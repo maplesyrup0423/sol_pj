6b28dea65c07bca3592b30b5841a349def56f8d2
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-03 03:15:24 +0900

    Writing 컴포넌트 table 태그 삭제 및css + 사용자의 입력에 따라 높이가 조절되는 textarea

--- pj/Client/src/components/pages/homeLayout/center/feed/Writing.jsx
+++ pj/Client/src/components/pages/homeLayout/center/feed/Writing.jsx
@@ -1,38 +1,42 @@
 import ProfileImg from "../../../../utills/ProfileImg";
 import "./Writing.css";
 import BasicButton from "../../../../utills/buttons/BasicButton";
+import { useState } from "react";
+
 function Writing({ userInfo, boardId }) {
+  const [, setContent] = useState("");
+
+  const handleInputChange = (event) => {
+    const textarea = event.target;
+    textarea.style.height = "auto"; // 높이를 자동으로 조정
+    textarea.style.height = textarea.scrollHeight + "px"; // 내용에 맞춰 높이 설정
+    setContent(textarea.value);
+  };
+
   return (
-    <div className="writingArea">
-      <table className="Wtable">
-        <tbody>
-          <tr>
-            <td className="ProfileImg">
-              <ProfileImg image_url={userInfo.image_url} />
-            </td>
-            <td className="">
-              <textarea
-                name="postContent"
-                rows={4}
-                cols={40}
-                placeholder={`게시판 아이디 : ${boardId} || 글을 작성해주세요.`}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={2} className="btn">
-              {/* 버튼 */}
-              <BasicButton
-                btnOn={false}
-                btnSize="mediumButton"
-                btnColor="yellowButton"
-                action={null}
-                btnText="전송"
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="write-container">
+      <div className="write-row">
+        <div className="profile-image">
+          <ProfileImg image_url={userInfo.image_url} />
+        </div>
+        <div className="textarea-container">
+          <textarea
+            onChange={handleInputChange}
+            name="postContent"
+            rows={2}
+            placeholder={`게시판 아이디 : ${boardId} || 글을 작성해주세요.`}
+          />
+        </div>
+      </div>
+      <div className="button-row">
+        <BasicButton
+          btnOn={false}
+          btnSize="mediumButton"
+          btnColor="yellowButton"
+          action={null}
+          btnText="전송"
+        />
+      </div>
     </div>
   );
 }
