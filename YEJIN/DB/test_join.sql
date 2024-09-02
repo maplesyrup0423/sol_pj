841f52fb899d072825491eb15dbdc3846a2a3a54
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-03 00:08:11 +0900

    댓글수 좋아요수 count 해와서 출력

--- YEJIN/DB/test_join.sql
+++ YEJIN/DB/test_join.sql
@@ -20,7 +20,7 @@ ORDER BY p.createDate DESC;
 
 
 SELECT p.post_id,p.post_text,p.user_no,p.createDate,p.modiDate, p.views,
-GROUP_CONCAT(pf.file_path ORDER BY pf.upload_date SEPARATOR ', ') AS file_paths,
+GROUP_CONCAT( DISTINCT pf.file_path ORDER BY pf.upload_date SEPARATOR ', ') AS file_paths,
 u.user_id,up.nickname,up.image_url,
 COUNT(DISTINCT l.p_like_id) AS like_count, 
 COUNT(DISTINCT c.comment_id) AS comment_count 
@@ -33,3 +33,5 @@ LEFT JOIN comments c ON p.post_id = c.post_id
 WHERE p.board_info_id = 1 and  p.isDeleted=0
 GROUP BY p.post_id, p.post_text, p.user_no, p.createDate, p.modiDate, p.views, u.user_id, up.nickname, up.image_url
 ORDER BY p.createDate DESC;
+
+
