36e84a570a72e2c6fd906410fe03a4cf8e0e267d
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-03 15:52:56 +0900

    게시글 insert (미완)

--- pj/Client/src/components/pages/homeLayout/center/feed/Writing.jsx
+++ pj/Client/src/components/pages/homeLayout/center/feed/Writing.jsx
@@ -2,41 +2,65 @@ import ProfileImg from "../../../../utills/ProfileImg";
 import "./Writing.css";
 import BasicButton from "../../../../utills/buttons/BasicButton";
 import { useState } from "react";
+import api from "../../../../auth/api";
 
 function Writing({ userInfo, boardId }) {
   const [, setContent] = useState("");
+  const [postContent, setPostContent] = useState("");
 
   const handleInputChange = (event) => {
     const textarea = event.target;
     textarea.style.height = "auto"; // 높이를 자동으로 조정
     textarea.style.height = textarea.scrollHeight + "px"; // 내용에 맞춰 높이 설정
     setContent(textarea.value);
+
+    setPostContent(event.target.value); //사용자 입력 값
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // 서버로 보낼 데이터 구성
+    const postData = {
+      postContent,
+      user_no: userInfo.user_no, // 유저 ID
+      board_info_id: boardId, // 게시판 ID
+    };
+
+    try {
+      const response = await api.post("/api/postInsert", postData);
+      console.log("Response:", response.data);
+    } catch (err) {
+      console.error("Error inserting post:", err);
+    }
+  };
   return (
     <div className="write-container">
-      <div className="write-row">
-        <div className="profile-image">
-          <ProfileImg image_url={userInfo.image_url} />
+      <form onSubmit={handleSubmit}>
+        <div className="write-row">
+          <div className="profile-image">
+            <ProfileImg image_url={userInfo.image_url} />
+          </div>
+          <div className="textarea-container">
+            <textarea
+              onChange={handleInputChange}
+              name="postContent"
+              rows={2}
+              placeholder={`게시판 아이디 : ${boardId} || 글을 작성해주세요.`}
+            />
+          </div>
         </div>
-        <div className="textarea-container">
-          <textarea
-            onChange={handleInputChange}
-            name="postContent"
-            rows={2}
-            placeholder={`게시판 아이디 : ${boardId} || 글을 작성해주세요.`}
+        <div className="button-row">
+          <BasicButton
+            btnOn={false}
+            btnSize="mediumButton"
+            btnColor="yellowButton"
+            action={null}
+            btnText="전송"
+            type="submit"
           />
         </div>
-      </div>
-      <div className="button-row">
-        <BasicButton
-          btnOn={false}
-          btnSize="mediumButton"
-          btnColor="yellowButton"
-          action={null}
-          btnText="전송"
-        />
-      </div>
+      </form>
     </div>
   );
 }
