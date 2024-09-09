8adeaa1a77ffad517b515198ee3f5df22d4e10ea
khkhk1 <gksruf1414@gmail.com>
2024-09-09 21:15:07 +0900

    ㅇㅇ

--- pj/Server/server.js
+++ pj/Server/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // 클라이언트 URL
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   })
