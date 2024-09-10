77cfc837c04279701c36dea8e9c0871d6aaa6ef0
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-10 20:09:35 +0900

    회원 이미지 부분 하드 코딩인 부분 수정해둠
    제가 만든 부분만 수정해뒀습니다.

--- pj/Client/src/components/utills/ProfileImg.jsx
+++ pj/Client/src/components/utills/ProfileImg.jsx
@@ -1,17 +1,17 @@
 function ProfileImg(props) {
-    return (
-        // 임시이미지
-        <img
-            src={props.image_url}
-            alt="프로필 이미지"
-            style={{
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                border: "1px black",
-            }}
-        />
-    );
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  return (
+    <img
+      src={`${baseUrl}/images/uploads/${props.image_url}`}
+      alt="프로필 이미지"
+      style={{
+        width: "50px",
+        height: "50px",
+        borderRadius: "50%",
+        border: "1px black",
+      }}
+    />
+  );
 }
 
 export default ProfileImg;
