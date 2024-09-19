e339834249479ab0ee5062314f3b38a42d36ef35
khkhk1 <gksruf1414@gmail.com>
2024-09-19 19:51:50 +0900

    프로필 게시글 내림차순정렬, 피드 수정버튼 가운데정렬

--- pj/Server/controllers/userPost.js
+++ pj/Server/controllers/userPost.js
@@ -7,18 +7,19 @@ module.exports = function (conn) {
     const user_no = req.params.user_no; // 요청 파라미터에서 user_no 가져오기
     console.log("요청된 user_no:", user_no);
     const query = `SELECT u.user_no, p.post_id, p.post_text, p.createDate, p.modiDate, p.views, 
-          u.user_id, up.nickname, up.image_url,
-          GROUP_CONCAT(DISTINCT pf.file_path ORDER BY pf.upload_date SEPARATOR ', ') AS file_paths,
-          COUNT(DISTINCT l.p_like_id) AS like_count,
-          COUNT(DISTINCT c.comment_id) AS comment_count, p.board_info_id
-          FROM posts p
-          LEFT JOIN post_files pf ON p.post_id = pf.post_id
-          LEFT JOIN User u ON p.user_no = u.user_no
-          LEFT JOIN UserProfile up ON u.user_no = up.user_no
-          LEFT JOIN post_likes l ON p.post_id = l.post_id
-          LEFT JOIN comments c ON p.post_id = c.post_id
-          WHERE u.user_no = ? AND p.isDeleted = 0
-          GROUP BY u.user_no, p.post_id, p.post_text, p.createDate, p.modiDate, p.views, u.user_id, up.nickname, up.image_url, p.board_info_id`;
+       u.user_id, up.nickname, up.image_url,
+       GROUP_CONCAT(DISTINCT pf.file_path ORDER BY pf.upload_date SEPARATOR ', ') AS file_paths,
+       COUNT(DISTINCT l.p_like_id) AS like_count,
+       COUNT(DISTINCT c.comment_id) AS comment_count
+FROM posts p
+LEFT JOIN post_files pf ON p.post_id = pf.post_id
+LEFT JOIN User u ON p.user_no = u.user_no
+LEFT JOIN UserProfile up ON u.user_no = up.user_no
+LEFT JOIN post_likes l ON p.post_id = l.post_id
+LEFT JOIN comments c ON p.post_id = c.post_id
+WHERE u.user_no = 3 AND p.isDeleted = 0
+GROUP BY u.user_no, p.post_id, p.post_text, p.createDate, p.modiDate, p.views, u.user_id, up.nickname, up.image_url
+ORDER BY p.createDate DESC`;
 
     conn.query(query, [user_no], (error, results) => {
       if (error) {
