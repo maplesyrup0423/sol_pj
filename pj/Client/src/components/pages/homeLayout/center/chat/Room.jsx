26deff0e8b2cd0e8f0dd29e5b476b755db8bd5e8
xxzz9978 <xxzz9978@naver.com>
2024-09-24 18:14:33 +0900

    수정전 확인용 psuh

--- pj/Client/src/components/pages/homeLayout/center/chat/Room.jsx
+++ pj/Client/src/components/pages/homeLayout/center/chat/Room.jsx
@@ -8,7 +8,7 @@ import ChatMessage from "./ChatMessage";
 import "./Room.css";
 
 function Room() {
-    const { userInfo, accessToken, setUserInfo } = useContext(AuthContext);
+    const { userInfo, accessToken, setUserInfo, activeRoom, setActiveRoom } = useContext(AuthContext);
     const { roomId } = useParams();
     const [socket, setSocket] = useState(null);
     const [messages, setMessages] = useState([]);
@@ -19,8 +19,6 @@ function Room() {
     const [page, setPage] = useState(1);
     const [hasMore, setHasMore] = useState(true);
     const [isInitialLoad, setIsInitialLoad] = useState(true);
-    const prevScrollTopRef = useRef(0); // 스크롤 위치 저장
-    const firstMessageRef = useRef(null); // 첫 번째 메시지 저장
 
     useEffect(() => {
         const checkUserInfo = async () => {
@@ -29,7 +27,7 @@ function Room() {
                     const response = await api.post('/api/user-info', {});
                     setUserInfo(response.data);
                 } catch (error) {
-                    console.error("유저정보를 가져올수가 없습니다. :", error);
+                    console.error("유저정보를 가져올 수 없습니다:", error);
                 }
             }
             setLoading(false);
@@ -38,72 +36,70 @@ function Room() {
         checkUserInfo();
     }, [userInfo, setUserInfo, accessToken]);
 
-    useEffect(() => {
-        if (userInfo && userInfo.user_no && accessToken) {
-            const newSocket = io('http://localhost:5000', {
-                auth: { token: accessToken }
-            });
-            setSocket(newSocket);
+   useEffect(() => {
+    
+    console.log("Room ID:", roomId); // roomId 확인
 
-            return () => newSocket.close();
-        }
-    }, [userInfo, accessToken]);
-
-    useEffect(() => {
-    if (socket && userInfo && userInfo.user_no) {
-        socket.emit('join_room', roomId, userInfo.user_no);
-
-        socket.on('initial_messages', (initialMessages) => {
-            console.log('초기 메시지:', initialMessages);
-    setMessages(initialMessages.reverse()); // 내림차순으로 저장
-    setHasMore(initialMessages.length === 20);
-    setIsInitialLoad(false);
-    scrollToBottom();
-});
-        socket.on('chat_message', (data) => {
-            setMessages((prevMessages) => {
-                const updatedMessages = [...prevMessages, data];
-                const isAtBottom = messageListRef.current.scrollHeight - messageListRef.current.clientHeight <= messageListRef.current.scrollTop + 1;
-                
-                if (isAtBottom) {
-                    scrollToBottom();
-                }
-                return updatedMessages;
-            });
+    if (userInfo && userInfo.user_no && accessToken) {
+        const newSocket = io('http://localhost:5000', {
+            auth: { token: accessToken }
         });
-
-        socket.on('additional_messages', (additionalMessages) => {
-    // 현재 스크롤 위치와 스크롤 높이를 기록
-    const prevScrollHeight = messageListRef.current.scrollHeight;
-    const prevScrollTop = messageListRef.current.scrollTop;
-            console.log('추가 메시지:', additionalMessages);
-            console.log('추가 메시지.reverse:', additionalMessages.reverse());
-    setMessages((prevMessages) => {
-        // 추가된 메시지를 위에 쌓이도록 변경
-        
-        
-         const newMessages = [...additionalMessages, ...prevMessages]; // 추가 메시지를 앞에 추가
-        return newMessages;
-    });
-
-    setTimeout(() => {
-        // 메시지 추가 후 스크롤 위치 복원
-        const newScrollHeight = messageListRef.current.scrollHeight;
-        messageListRef.current.scrollTop = newScrollHeight - prevScrollHeight + prevScrollTop;
-    }, 0);  // 바로 적용되도록 짧은 딜레이 사용
-
-    setHasMore(additionalMessages.length === 20);
-});
-
-
+        setSocket(newSocket);
+        setActiveRoom(roomId); // roomId를 activeRoom으로 설정
+        console.log(activeRoom)
+        console.log("활성 방 설정:", roomId); // 방 ID 확인
         return () => {
-            socket.off('initial_messages');
-            socket.off('chat_message');
-            socket.off('additional_messages');
+            newSocket.close();
+            setActiveRoom(null);
         };
     }
-}, [socket, userInfo, isInitialLoad, roomId]);
+}, [userInfo, accessToken,roomId]); 
 
+    useEffect(() => {
+        if (socket && userInfo && userInfo.user_no) {
+            socket.emit('join_room', roomId, userInfo.user_no);
+
+            socket.on('initial_messages', (initialMessages) => {
+                console.log('초기 메시지:', initialMessages);
+                setMessages(initialMessages.reverse());
+                setHasMore(initialMessages.length === 20);
+                setIsInitialLoad(false);
+                scrollToBottom();
+            });
+
+            socket.on('chat_message', (data) => {
+                setMessages((prevMessages) => {
+                    const updatedMessages = [...prevMessages, data];
+                    const isAtBottom = messageListRef.current.scrollHeight - messageListRef.current.clientHeight <= messageListRef.current.scrollTop + 1;
+                    
+                    if (isAtBottom) {
+                        scrollToBottom();
+                    }
+                    return updatedMessages;
+                });
+            });
+
+            socket.on('additional_messages', (additionalMessages) => {
+                const prevScrollHeight = messageListRef.current.scrollHeight;
+                const prevScrollTop = messageListRef.current.scrollTop;
+                console.log('추가 메시지:', additionalMessages);
+                setMessages((prevMessages) => [...additionalMessages, ...prevMessages]);
+                
+                setTimeout(() => {
+                    const newScrollHeight = messageListRef.current.scrollHeight;
+                    messageListRef.current.scrollTop = newScrollHeight - prevScrollHeight + prevScrollTop;
+                }, 0);
+                
+                setHasMore(additionalMessages.length === 20);
+            });
+
+            return () => {
+                socket.off('initial_messages');
+                socket.off('chat_message');
+                socket.off('additional_messages');
+            };
+        }
+    }, [socket, userInfo, roomId]);
 
     const scrollToBottom = () => {
         if (messageListRef.current) {
@@ -130,19 +126,19 @@ function Room() {
     };
 
     const loadMoreMessages = () => {
-    if (socket && userInfo && userInfo.user_no && hasMore) {
-        const oldestMessage = messages[messages.length - 1]; // 가장 오래된 메시지를 가져오도록 수정
-        const oldestMessageDate = oldestMessage ? new Date(oldestMessage.created_at) : new Date();
+        if (socket && userInfo && userInfo.user_no && hasMore) {
+            const oldestMessage = messages[messages.length - 1];
+            const oldestMessageDate = oldestMessage ? new Date(oldestMessage.created_at) : new Date();
 
-        socket.emit('fetch_more_messages', {
-            room_id: roomId,
-            joined_at: oldestMessageDate.toISOString(),
-            page: page + 1,
-            pageSize: 20
-        });
-        setPage(prevPage => prevPage + 1); // 페이지 증가
-    }
-};
+            socket.emit('fetch_more_messages', {
+                room_id: roomId,
+                joined_at: oldestMessageDate.toISOString(),
+                page: page + 1,
+                pageSize: 20
+            });
+            setPage(prevPage => prevPage + 1);
+        }
+    };
 
     const handleScroll = () => {
         const { scrollTop } = messageListRef.current;
