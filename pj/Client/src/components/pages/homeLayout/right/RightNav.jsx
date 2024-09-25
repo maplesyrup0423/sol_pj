0a18f9c2defa5025300d88c74afa82b513e4d210
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-25 23:31:07 +0900

    채팅방 목록 css 및 각종 버튼 hover 적용

--- pj/Client/src/components/pages/homeLayout/right/RightNav.jsx
+++ pj/Client/src/components/pages/homeLayout/right/RightNav.jsx
@@ -4,77 +4,77 @@ import Notice from "./Notice";
 import Messenger from "./Messenger";
 import Footer from "./Footer";
 import { useState, useContext, useEffect } from "react";
-import { BsChatLeftDotsFill } from "react-icons/bs";
 import { FaUserGroup } from "react-icons/fa6";
 import { AuthContext } from "../../../../Context/AuthContext";
 import Followers from "./Followers";
 import { getFollowers } from "../../../utills/FollowService";
+import { LuMail } from "react-icons/lu";
 
 function RightNav() {
-    const { userInfo } = useContext(AuthContext);
-    const [followerList, setFollowerList] = useState([]);
-    const [activeTab, setActiveTab] = useState("messenger");
+  const { userInfo } = useContext(AuthContext);
+  const [followerList, setFollowerList] = useState([]);
+  const [activeTab, setActiveTab] = useState("messenger");
 
-    // 유저 정보가 바뀌거나 activeTab이 "followers"로 바뀌면 팔로워 목록 불러오기
-    useEffect(() => {
-        if (userInfo && activeTab === "followers") {
-            // 팔로워 목록 불러오기
-            const fetchFollowers = async () => {
-                const list = await getFollowers(userInfo.user_no);
-                setFollowerList(list);
-            };
-            fetchFollowers();
-        }
-    }, [userInfo, activeTab]);
+  // 유저 정보가 바뀌거나 activeTab이 "followers"로 바뀌면 팔로워 목록 불러오기
+  useEffect(() => {
+    if (userInfo && activeTab === "followers") {
+      // 팔로워 목록 불러오기
+      const fetchFollowers = async () => {
+        const list = await getFollowers(userInfo.user_no);
+        setFollowerList(list);
+      };
+      fetchFollowers();
+    }
+  }, [userInfo, activeTab]);
 
-    const handleTabChange = (tabName) => {
-        setActiveTab(tabName);
-    };
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  };
 
-    return (
-        <div className="rsidebar">
-            <main>
-                <ul>
-                    <li>
-                        <SearchBox />
-                    </li>
-                    <li>
-                        <Notice user_no={userInfo.user_no} />
-                    </li>
-                    <li>
-                        <div className="activeTab">
-                            <button
-                                className="activeChat"
-                                onClick={() => handleTabChange("messenger")}
-                            >
-                                <BsChatLeftDotsFill size={45} />
-                            </button>
-                            <button
-                                className="activeChat"
-                                onClick={() => {
-                                    if (userInfo && userInfo.user_no) {
-                                        handleTabChange("followers");
-                                    } else {
-                                        console.error(
-                                            "유저 정보가 아직 로드되지 않았습니다."
-                                        );
-                                    }
-                                }}
-                                disabled={!userInfo || !userInfo.user_no}
-                            >
-                                <FaUserGroup size={45} />
-                            </button>
-                        </div>
-                        {activeTab === "messenger" && <Messenger />}
-                        {activeTab === "followers" && (
-                            <Followers followerList={followerList} />
-                        )}
-                    </li>
-                </ul>
-            </main>
-            <Footer />
+  return (
+    <div className="rsidebar">
+      <main>
+        <ul>
+          <li>
+            <SearchBox />
+          </li>
+          <li>
+            <Notice user_no={userInfo.user_no} />
+          </li>
+          <li>
+            <div className="activeTab">
+              <button
+                className="activeChat"
+                onClick={() => handleTabChange("messenger")}
+              >
+                <LuMail size={40} />
+              </button>
+              <button
+                className="activeChat"
+                onClick={() => {
+                  if (userInfo && userInfo.user_no) {
+                    handleTabChange("followers");
+                  } else {
+                    console.error("유저 정보가 아직 로드되지 않았습니다.");
+                  }
+                }}
+                disabled={!userInfo || !userInfo.user_no}
+              >
+                <FaUserGroup size={40} />
+              </button>
+            </div>
+            {activeTab === "messenger" && <Messenger />}
+            {activeTab === "followers" && (
+              <Followers followerList={followerList} />
+            )}
+          </li>
+        </ul>
+        <div className="RightNav-Footer">
+          <Footer />
         </div>
-    );
+      </main>
+    </div>
+  );
 }
 
 export default RightNav;
