7f05cb669f9c7b9cd11ac7a5b5ff07ca22cced98
xxzz9978 <xxzz9978@naver.com>
2024-09-25 20:29:52 +0900

    알림 재재재수정

--- pj/Client/src/components/pages/homeLayout/right/Messenger.jsx
+++ pj/Client/src/components/pages/homeLayout/right/Messenger.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState, useEffect, useContext, useCallback } from "react";
+import { Link, NavLink, useParams } from "react-router-dom";
 import "./Messenger.css";
 import MessengerUser from "./MessengerUser";
 import { FaPlus } from "react-icons/fa";
@@ -7,7 +7,8 @@ import io from "socket.io-client";
 import { AuthContext } from "../../../../Context/AuthContext";
 
 function Messenger() {
-  const { userInfo, activeRoom, setActiveRoom } = useContext(AuthContext);
+  const { userInfo } = useContext(AuthContext);
+  const { roomId: activeRoomId } = useParams(); // URL에서 현재 활성화된 방 ID를 가져옵니다
   const [chatList, setChatList] = useState([]);
   const [socket, setSocket] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -22,27 +23,89 @@ function Messenger() {
     );
   };
 
-  const formatLastDate = (dateString) => {
+  const formatLastDate = useCallback((dateString) => {
     const date = new Date(dateString);
     if (isNaN(date)) {
       console.error("유효하지 않은 날짜 형식:", dateString);
       return dateString;
     }
     const options = { hour: "2-digit", minute: "2-digit", hour12: true };
-    const kstOffset = 9 * 60 * 60 * 1000;
-    const localDate = new Date(date.getTime() + kstOffset);
 
-    if (isToday(localDate)) {
-      return localDate.toLocaleTimeString("ko-KR", options);
+    if (isToday(date)) {
+      return date.toLocaleTimeString("ko-KR", options);
     } else {
-      const month = String(localDate.getMonth() + 1).padStart(2, "0");
-      const day = String(localDate.getDate()).padStart(2, "0");
-      return `${month}/${day} ${localDate.toLocaleTimeString(
-        "ko-KR",
-        options
-      )}`;
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      return `${month}/${day} ${date.toLocaleTimeString("ko-KR", options)}`;
     }
-  };
+  }, []);
+
+  const updateChatList = useCallback(
+    (updatedChatList) => {
+      const updatedList = updatedChatList.map((chat) => ({
+        ...chat,
+        room_name:
+          chat.room_name === userInfo.nickname
+            ? chat.last_sender
+            : chat.room_name,
+        last_date: formatLastDate(chat.last_date),
+        unread_count:
+          chat.room_id.toString() === activeRoomId ? 0 : chat.unread_count,
+      }));
+      setChatList(updatedList);
+      setIsLoading(false);
+    },
+    [userInfo.nickname, formatLastDate, activeRoomId]
+  );
+
+  const handleNewMessage = useCallback(
+    (data) => {
+      const roomId = Number(data.room_id);
+      const formattedDate = formatLastDate(data.last_message.created_at);
+      const isSender = data.last_message.nickname === userInfo.nickname;
+      const isActiveRoom = roomId.toString() === activeRoomId;
+
+      setChatList((prevList) => {
+        const existingChatIndex = prevList.findIndex(
+          (chat) => chat.room_id === roomId
+        );
+        if (existingChatIndex !== -1) {
+          const updatedList = [...prevList];
+          updatedList[existingChatIndex] = {
+            ...updatedList[existingChatIndex],
+            unread_count:
+              isActiveRoom || isSender
+                ? 0
+                : updatedList[existingChatIndex].unread_count + 1,
+            last_date: formattedDate,
+            last_chat: data.last_message.message_content,
+            last_sender: data.last_message.nickname,
+            room_name:
+              updatedList[existingChatIndex].room_name === userInfo.nickname
+                ? data.last_message.nickname
+                : updatedList[existingChatIndex].room_name,
+          };
+          return updatedList;
+        } else {
+          return [
+            ...prevList,
+            {
+              room_id: roomId,
+              room_name:
+                userInfo.nickname === data.last_message.nickname
+                  ? data.room_name
+                  : data.last_message.nickname,
+              unread_count: isSender || isActiveRoom ? 0 : 1,
+              last_date: formattedDate,
+              last_chat: data.last_message.message_content,
+              last_sender: data.last_message.nickname,
+            },
+          ];
+        }
+      });
+    },
+    [userInfo.nickname, activeRoomId, formatLastDate]
+  );
 
   useEffect(() => {
     const newSocket = io("http://localhost:5000/chatNotification", {
@@ -62,62 +125,8 @@ function Messenger() {
       loadChatList();
     });
 
-    newSocket.on("chat_list_update", (updatedChatList) => {
-      const updatedList = updatedChatList.map((chat) => ({
-        ...chat,
-        room_name:
-          chat.room_name === userInfo.nickname
-            ? chat.last_sender
-            : chat.room_name,
-        last_date: formatLastDate(chat.last_date),
-      }));
-      setChatList(updatedList);
-      setIsLoading(false);
-    });
-
-    newSocket.on("new_message", (data) => {
-      const roomId = Number(data.room_id);
-      const chat = chatList.find((chat) => chat.room_id === roomId);
-      const formattedDate = formatLastDate(data.last_message.created_at);
-      const isSender = data.last_message.nickname === userInfo.nickname;
-      const isActiveRoom = roomId === Number(activeRoom);
-
-      setChatList((prevList) => {
-        if (chat) {
-          return prevList.map((chat) =>
-            chat.room_id === roomId
-              ? {
-                  ...chat,
-                  unread_count:
-                    isActiveRoom || isSender ? 0 : chat.unread_count + 1,
-                  last_date: formattedDate,
-                  last_chat: data.last_message.message_content,
-                  last_sender: data.last_message.nickname,
-                  room_name:
-                    chat.room_name === userInfo.nickname
-                      ? data.last_message.nickname
-                      : chat.room_name,
-                }
-              : chat
-          );
-        } else {
-          return [
-            ...prevList,
-            {
-              room_id: roomId,
-              room_name:
-                userInfo.nickname === data.last_message.nickname
-                  ? data.room_name
-                  : data.last_message.nickname,
-              unread_count: isSender || isActiveRoom ? 0 : 1,
-              last_date: formattedDate,
-              last_chat: data.last_message.message_content,
-              last_sender: data.last_message.nickname,
-            },
-          ];
-        }
-      });
-    });
+    newSocket.on("chat_list_update", updateChatList);
+    newSocket.on("new_message", handleNewMessage);
 
     newSocket.on("error", (errorMessage) => {
       console.error("소켓 에러:", errorMessage);
@@ -128,22 +137,22 @@ function Messenger() {
     return () => {
       newSocket.disconnect();
     };
-  }, [userInfo.user_no, activeRoom]); // activeRoom을 의존성 배열에 추가
+  }, [userInfo.user_no, updateChatList, handleNewMessage]);
 
   useEffect(() => {
-    if (activeRoom) {
+    if (activeRoomId) {
       setChatList((prevList) =>
         prevList.map((chat) =>
-          chat.room_id === Number(activeRoom)
+          chat.room_id.toString() === activeRoomId
             ? { ...chat, unread_count: 0 }
             : chat
         )
       );
     }
-  }, [activeRoom, chatList]);
+  }, [activeRoomId]);
 
   if (isLoading) {
-    return;
+    return <div>로딩 중...</div>;
   }
 
   if (error) {
