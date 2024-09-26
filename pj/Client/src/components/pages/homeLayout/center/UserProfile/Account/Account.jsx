214aa0ad3149fb549b0f94e2cb467ce9c5a0c3ef
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-26 23:58:36 +0900

    Account 컴포넌트 중복 코드 최적화

--- pj/Client/src/components/pages/homeLayout/center/UserProfile/Account/Account.jsx
+++ pj/Client/src/components/pages/homeLayout/center/UserProfile/Account/Account.jsx
@@ -8,6 +8,32 @@ import { MdOutlineSwitchAccount } from "react-icons/md";
 import { TiDeleteOutline } from "react-icons/ti";
 
 function Account() {
+  const menuItems = [
+    {
+      title: "계정정보",
+      description: "계정정보를 조회합니다.",
+      icon: <VscAccount color="gray" size={25} />,
+      path: "",
+    },
+    {
+      title: "비밀번호 변경",
+      description: "비밀번호를 변경합니다.",
+      icon: <LuKeyRound color="gray" size={25} />,
+      path: "/ChangePassword",
+    },
+    {
+      title: "로그인 기록 조회",
+      description: "로그인한 기록을 조회합니다.",
+      icon: <MdOutlineSwitchAccount color="gray" size={25} />,
+      path: "/LoginHistory",
+    },
+    {
+      title: "계정 비활성화",
+      description: "계정을 비활성화 합니다.",
+      icon: <TiDeleteOutline color="gray" size={25} />,
+      path: "",
+    },
+  ];
   const navigate = useNavigate();
   const handleBack = () => {
     navigate(-1); //뒤로가기
@@ -23,67 +49,26 @@ function Account() {
       </div>
       <div className="Account-body">
         <ul>
-          <li>
-            <NavLink to={``} className="Account-NavLink">
-              <div className="Account-btn">
-                <div className="Account-left">
-                  <VscAccount color="gray" size={25} />
-                  <div className="Account-text">
-                    <p>계정정보</p>
-                    <p className="Account-gray-text">계정정보를 조회합니다.</p>
+          {menuItems.map((item, index) => (
+            <li key={index}>
+              <NavLink to={item.path} className="Account-NavLink">
+                <div className="Account-btn">
+                  <div className="Account-left">
+                    {item.icon}
+                    <div className="Account-text">
+                      <p>{item.title}</p>
+                      <p className="Account-gray-text">{item.description}</p>
+                    </div>
                   </div>
+                  <IoIosArrowForward color="gray" size={25} />
                 </div>
-                <IoIosArrowForward color="gray" size={25} />
-              </div>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/ChangePassword"} className="Account-NavLink">
-              <div className="Account-btn">
-                <div className="Account-left">
-                  <LuKeyRound color="gray" size={25} />
-                  <div className="Account-text">
-                    <p>비밀번호 변경</p>
-                    <p className="Account-gray-text">비밀번호를 변경합니다.</p>
-                  </div>
-                </div>
-                <IoIosArrowForward color="gray" size={25} />
-              </div>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/LoginHistory"} className="Account-NavLink">
-              <div className="Account-btn">
-                <div className="Account-left">
-                  <MdOutlineSwitchAccount color="gray" size={25} />
-                  <div className="Account-text">
-                    <p>로그인 기록 조회</p>
-                    <p className="Account-gray-text">
-                      로그인한 기록을 조회합니다.
-                    </p>
-                  </div>
-                </div>
-                <IoIosArrowForward color="gray" size={25} />
-              </div>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={``} className="Account-NavLink">
-              <div className="Account-btn">
-                <div className="Account-left">
-                  <TiDeleteOutline color="gray" size={25} />
-                  <div className="Account-text">
-                    <p>계정 비활성화</p>
-                    <p className="Account-gray-text">계정을 비활성화 합니다.</p>
-                  </div>
-                </div>
-                <IoIosArrowForward color="gray" size={25} />
-              </div>
-            </NavLink>
-          </li>
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
     </>
   );
 }
+
 export default Account;
