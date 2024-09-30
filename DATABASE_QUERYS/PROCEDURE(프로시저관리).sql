f65c079ba3e5875364ad995e69d64b294af3b034
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-30 19:57:26 +0900

    CREATE PROCEDURE 파일 통일

--- DATABASE_QUERYS/PROCEDURE(프로시저관리).sql
+++ DATABASE_QUERYS/PROCEDURE(프로시저관리).sql
@@ -98,6 +98,41 @@ DELIMITER ;
 
 
 
+-- DROP PROCEDURE IF EXISTS UpdateUserBoard;
+-- 커스텀 게시판 관련 프로시저
+DELIMITER //
+-- UserBoard 값을 클라이언트에서 선택한값으로 insert 하는 프로시저
+CREATE PROCEDURE UpdateUserBoard(
+    IN p_user_no INT,
+    IN p_board_ids JSON
+)
+BEGIN
+    -- 트랜잭션 시작
+    START TRANSACTION;
+
+    -- 기존 사용자 게시판 데이터 삭제
+    DELETE FROM UserBoard
+    WHERE user_no = p_user_no
+      AND board_info_id NOT IN (
+        SELECT value
+        FROM JSON_TABLE(p_board_ids, '$[*]' COLUMNS (value INT PATH '$')) AS jt
+    );
+
+    -- 새로운 게시판 데이터 삽입
+    INSERT INTO UserBoard (user_no, board_info_id)
+    SELECT p_user_no, value
+    FROM JSON_TABLE(p_board_ids, '$[*]' COLUMNS (value INT PATH '$')) AS jt
+    ON DUPLICATE KEY UPDATE board_info_id = VALUES(board_info_id);
+
+    -- 트랜잭션 커밋
+    COMMIT;
+END //
+
+DELIMITER ;
+
+-- 프로시터 테스트
+ -- CALL UpdateUserBoard(1, '[1,2,3]');
+-- select board_info_id from userboard where user_no=1;
 
 
 SHOW PROCEDURE STATUS WHERE Db = 'sol';
