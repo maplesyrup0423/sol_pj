9c75b9785c0a58cb9f2f3271265a738b2308b5a0
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-10-22 11:30:18 +0900

    댓글수 표기 : 삭제된 댓글까지 count하면 이슈 해결

--- pj/Server/controllers/postDetail.js
+++ pj/Server/controllers/postDetail.js
@@ -38,7 +38,7 @@ module.exports = (conn) => {
           u.user_id, up.nickname, up.image_url,
           GROUP_CONCAT(DISTINCT pf.file_path ORDER BY pf.upload_date SEPARATOR ', ') AS file_paths,
           COUNT(DISTINCT l.p_like_id) AS like_count,
-          COUNT(DISTINCT c.comment_id) AS comment_count
+           COUNT(DISTINCT CASE WHEN c.isDeleted = 0 THEN c.comment_id END) AS comment_count
           FROM posts p
           LEFT JOIN post_files pf ON p.post_id = pf.post_id
           LEFT JOIN User u ON p.user_no = u.user_no
